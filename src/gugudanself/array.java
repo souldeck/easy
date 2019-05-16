2c07d6a9af764c20442851abcca52aae13cfbe87
이경하 <@DESKTOP-GUMUV5A>
2019-05-16 17:35:28 +0900

    id로그인 어플리케이션;cmd로 실행시키기

--- src/gugudanself/array.java
+++ src/gugudanself/array.java
@@ -2,23 +2,17 @@ package gugudanself;
 
 public class array {
 	public static void main(String[] args) {
-		//조건문
-		if(true) { //true 안을 가변적으로 만들어줘야 의미있는 코드.
-			//만약에 if 뒤에 오는 괄호 안에 들어오는 값이 참이라면 if 따라 오는 중괄호 안의 코드가 실행된다.
-			//if () 괄호안에 들어올 명령문은 boolean 외에는 못들어옴. (true, false)
-			System.out.println("헬로월드;dhs");
-		} if(false){ // = } else { 로 표현할 수도 있음. 
-			System.out.println("false");
+		//조건문을 이용한 로그인 어플리케이션 만들기
+		String id= args[0]; //입력값 받기
+		if(id.equals("RSM")) {
+			System.out.println("welcome! RSM");
+		} else {
+			System.out.println("wrong");
 		}
-		
-	   boolean flag = false; //가변값 지정
-	   if(flag) { //falg = true 일때 중괄호 안 코드가 실행됨 
-		   System.out.println("true");
-	  // 틀린 코드 } else(flag) {  
-	   } else { //flag = false 로 지정할 경우 중괄호 안 코드가 실행됨 	   
-		   System.out.println("false");
-	   }
-	   
+/* 이 java 프로그램을 cmd ~bin 디렉토리에서 
+ * java packageName.javaName (지정한 아이디)RSM 명령문을 실행시킨다. 
+ * 아이디가 if문의 조건을 만족시키면 welcome 코드가, 아닐 시 wrong 코드가 실행된다.
+ */
 	   
 	}
 }
