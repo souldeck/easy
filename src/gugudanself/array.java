bc407948f1ada54572e83ab8ccebc72093286d67
souldeck <45283167+souldeck@users.noreply.github.com>
2019-05-17 03:23:45 +0900

    메소드 복수 입력

--- src/gugudanself/array.java
+++ src/gugudanself/array.java
@@ -2,20 +2,21 @@ package gugudanself;
 
 public class array {
 	//메소드의 정의 부분
-	public static void numbering(){ 
-		//메소드의 이름을 numbering이라 지정해줌 
-		// numbering() 소괄호가 비어있어 함수의 결과물을 저장가능
-		int i=0;
-		while(i<10) {
-			System.out.println(i); //결과값 출력
-			i++;
+	public static void numbering(int start, int fin){
+		int i = start;
+		int j = fin;
+			while(i<j) {
+				System.out.println(i);
+				i++;
+			}
+
 		}
-	}
+	
 	//메소드의 호출부분
 	public static void main(String[] args) {
-		numbering();  
-		numbering();
-		numbering();
+		numbering(3, 7);  
+		numbering(2, 5);
+		numbering(1, 3);
 		
 	}
 }
