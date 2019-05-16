b9971b40fc44f9ebc2a1ae41b6288c8508b070b6
이경하 <@DESKTOP-GUMUV5A>
2019-05-16 17:17:46 +0900

    조건문

--- src/gugudanself/array.java
+++ src/gugudanself/array.java
@@ -2,26 +2,23 @@ package gugudanself;
 
 public class array {
 	public static void main(String[] args) {
-		int i1 = -5;
-		int i2 = -i1;
-		int i3 = -i1; 
-		System.out.println("i3=" +i3);
-		//전위증감연산자. 값이 먼저 증가한 후 대입.
-		System.out.println("전위증감연산자");
-		System.out.println("i4=++i3");
-		int i4 = ++i3; //i3이 6으로 증가, i4에 6대입.
-		//i3 = i3 + 1; 
-		System.out.println("i3=" + i3);
-		System.out.println("i4=" + i4);
+		//조건문
+		if(true) { //true 안을 가변적으로 만들어줘야 의미있는 코드.
+			//만약에 if 뒤에 오는 괄호 안에 들어오는 값이 참이라면 if 따라 오는 중괄호 안의 코드가 실행된다.
+			//if () 괄호안에 들어올 명령문은 boolean 외에는 못들어옴. (true, false)
+			System.out.println("헬로월드;dhs");
+		} if(false){ // = } else { 로 표현할 수도 있음. 
+			System.out.println("false");
+		}
 		
-		
-		//후위 증감 연산자. 대입후 값이 증가. 
-				System.out.println("후위증감연산자");
-				System.out.println("i3=" + i3);
-				System.out.println("i5=i3++");
-				int i5 = i3++; 
-				System.out.println("i3=" + i3);
-				System.out.println("i5=" + i5);
-		
+	   boolean flag = false; //가변값 지정
+	   if(flag) { //falg = true 일때 중괄호 안 코드가 실행됨 
+		   System.out.println("true");
+	  // 틀린 코드 } else(flag) {  
+	   } else { //flag = false 로 지정할 경우 중괄호 안 코드가 실행됨 	   
+		   System.out.println("false");
+	   }
+	   
+	   
 	}
 }
