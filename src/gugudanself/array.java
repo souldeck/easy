6b8a068089e3b69eac2696ec39dda5b930cba4b7
souldeck <45283167+souldeck@users.noreply.github.com>
2019-05-16 18:04:20 +0900

    반복문

--- src/gugudanself/array.java
+++ src/gugudanself/array.java
@@ -2,23 +2,12 @@ package gugudanself;
 
 public class array {
 	public static void main(String[] args) {
-		//조건문을 이용한 로그인 어플리케이션 만들기
-		String id= args[0]; //입력값 받기
-		String pw= args[1];
-		if(id.equals("RSM")) {
-			if(pw.equals("1215")) {
-				System.out.println("welcome! RSM");
-			} else {
-				System.out.println("wrong");
-			}
-			
-		} else {
-			System.out.println("wrong");
+		//반복문
+		int i = 0; //값 초기화. 통상적으로 i는 반복문을 종료시키기 위한 변수값 지정문자로 쓰임. 
+		while(i<10) { // while: 괄호 안의 조건이 true일때 코드를 반복시킨다.
+			System.out.println("hello world");
+			i=++i;
+			//i=i++ 일시 i=0으로 무한 반복됨. 
 		}
-/* 이 java 프로그램을 cmd ~bin 디렉토리에서 
- * java packageName.javaName (지정한 아이디)RSM (지정한비밀번호)1215 명령문을 실행시킨다. 
- * 아이디가 if문의 조건을 만족시키면 welcome 코드가, 아닐 시 wrong 코드가 실행된다.
- */
-	   
 	}
 }
