29f44dd46ca4062cae81411bccded8696da183b1
souldeck <45283167+souldeck@users.noreply.github.com>
2019-05-17 03:32:32 +0900

    메소드 -출력을 호출에서

--- src/gugudanself/array.java
+++ src/gugudanself/array.java
@@ -1,23 +1,28 @@
 package gugudanself;
 
 public class array {
-	//메소드의 정의 부분
-	public static void numbering(int start, int fin){
+	//메소드 정의에서 sysout을 호출로 빼내보자
+	public static  String numbering(int start, int fin){
 		int i = start;
-		int j = fin;
-			while(i<j) {
-				System.out.println(i);
+		String output=""; //빈 이름 지정
+			while(i<fin) {
+				output+=i;
+				// output 빈이름에 i에 지정된 숫자를 계속 더해주자. 
+				// i<fin될때까지
 				i++;
 			}
+			 return output; //결과값을 되돌려준다. 라는 의미
 
 		}
 	
 	//메소드의 호출부분
 	public static void main(String[] args) {
-		numbering(3, 7);  
-		numbering(2, 5);
-		numbering(1, 3);
-		
+		String result=numbering(2, 8);
+		System.out.println(result);
+		//2이상 8미만까지의 숫자 출력
+		result=numbering(5,11);
+		System.out.println(result);
+		//5이상 11미만까지의 숫자를 출력해 result에 덮어씌우기
 	}
 }
 		
