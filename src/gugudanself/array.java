a79069d292ed6fa8853f2ad903d352d2dfba45e4
souldeck <45283167+souldeck@users.noreply.github.com>
2019-05-17 03:12:52 +0900

    메소드

--- src/gugudanself/array.java
+++ src/gugudanself/array.java
@@ -1,34 +1,22 @@
 package gugudanself;
 
 public class array {
-	public static void main(String[] args) {
-		// 배열 만들기
-		// 배열의 이름은 클래스 그룹. 각각의 이름 지정. 크기는 3
-		String[] classGroup = { "이경하", "오경화", "로버"} ;
-		// 배열 꺼내기
-		System.out.println(classGroup[0]);
-		System.out.println(classGroup[1]);
-		System.out.println(classGroup[2]);
-//에러     System.out.println(classGroup[3]); 
-	
-		// 배열의 크기를 아라보자		
-		String[] classGroup2 = new String[4];
-		classGroup[0] = "이경하"; //값을 초기화 시키는 과정 
-		System.out.println(classGroup2.length);
-		System.out.println(classGroup2[1]);//=null. 값을 지정해주지 않은 배열.
-		classGroup[1] = "오경화";
-		System.out.println(classGroup2.length);
-		classGroup[2] = "로버";
-		System.out.println(classGroup2.length);
-	
-	    // 배열과 반복문의 결합
-		String[] members = {"냥식","냥순","쌔미"};
-		for(int i=0; i<members.length;i++) { 
-			//i<3일때까지 i[n] 배열 안의 결과값을 출력하라
-			String member = members[i];
-			System.out.println(member+"고양이가 사료를 먹었습니다.");
+	//메소드의 정의 부분
+	public static void numbering(){ 
+		//메소드의 이름을 numbering이라 지정해줌 
+		// numbering() 소괄호가 비어있어 함수의 결과물을 저장가능
+		int i=0;
+		while(i<10) {
+			System.out.println(i); //결과값 출력
+			i++;
 		}
 	}
+	//메소드의 호출부분
+	public static void main(String[] args) {
+		numbering();  
+		numbering();
+		numbering();
+		
+	}
 }
-
 		
